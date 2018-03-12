919bff87d78f508418648052a883746333e07996
AzizAbdullaev <excellent.bek@gmail.com>
2018-03-12 14:34:24 +0200

    JQuery, Adaptiv, Cross-browser

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-menu.onclick = function hamburger(){
-	if (navigation.className === "nav-menu") navigation.className += " responsive";
-	else navigation.className = "nav-menu";
-}
+// Menu
+$('.mobile-menu').hide();
+
+$('#menu').on('click', function(){
+	$('.mobile-menu').slideToggle();
+})
+// End Menu
+
+// Back to top
+var $btnTop = $('.btn-top')
+$(window).on('scroll', function(){
+	if ($(window).scrollTop() >= 20){
+		$btnTop.fadeIn();
+	}else{
+		$btnTop.fadeOut();
+	}
+});
+
+$btnTop.on('click', function(){
+	$('html,body').animate({scrollTop:0}, 900)
+});
+// End Back to top
+
+//Scroll
+$(document).ready(function(){
+    $("#navigation, #nav-mobile").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+// End Scroll
